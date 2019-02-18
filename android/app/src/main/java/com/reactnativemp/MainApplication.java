cf7ae032c7a0a6f40c97f2590f67a3fe0894c90b
Feboll <klaus24128@gmail.com>
2019-02-18 14:13:49 +0300

    Add lib

--- android/app/src/main/java/com/reactnativemp/MainApplication.java
+++ android/app/src/main/java/com/reactnativemp/MainApplication.java
@@ -4,9 +4,10 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
-import com.airbnb.android.react.lottie.LottiePackage;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.wix.reactnativenotifications.RNNotificationsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.airbnb.android.react.lottie.LottiePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -27,10 +28,11 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+              new RNNotificationsPackage(MainApplication.this),
             new SplashScreenReactPackage(),
-            new LottiePackage(),
+            new RNGestureHandlerPackage(),
             new RNDeviceInfo(),
-            new RNGestureHandlerPackage()
+            new LottiePackage()
       );
     }
 
